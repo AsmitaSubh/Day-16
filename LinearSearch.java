e3523a7d7df94e9c664d9ac3eaf764bf5abb5e50
AsmitaSubh <saswain92@gmail.com>
2023-06-15 18:17:56 +0530

    LinearSearch

---
+++ LinearSearch.java
@@ -0,0 +1,24 @@
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 3, 1, 7};
+        int target = 3;
+
+        int index = linearSearch(arr, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+}
+
+
